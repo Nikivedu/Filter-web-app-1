0c116b5e54b1a475b138180c64474a5d6d17c9d0
Nikivedu <146697889+Nikivedu@users.noreply.github.com>
2024-06-01 12:17:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload(){
+
+}
+
+    function setup()
+    {
+        canvas = createCanvas(400, 300);
+        canvas.center();
+    }
+
+    function draw()
+    {}
+
+    function take_snapshot()
+    {
+        save('myFilterImg.png');
+    }
